5ad02de1521e3b92c1caaea54e9c4ae7e72ed576
BenYang@LAPTOP-9AGC2Q57 <ben.yang77@gmail.com>
2020-05-19 18:01:18 +0800

    add PersonalInfoBiz.cs & Repotory.cs

---
+++ WebApplication1/Biz/PersonalInfoBiz.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+using WebApplication1.Repotory;
+
+namespace WebApplication1.Biz
+{
+    public class PersonalInfoBiz
+    {
+        public List<PersonalInfoModel> getList()
+        {
+            var oPInfo = new PersonalInfoModel();
+            return new Repotory<PersonalInfoModel>().getList(oPInfo);
+        }
+    }
+}
---
+++ WebApplication1/Repotory/Repotory.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Repotory
+{
+    public class Repotory<T> where T : class
+    {
+        string SqlConnStr { get; }
+        public Repotory() { SqlConnStr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\TestingProject\Provision\AspNetWebApp\WebApplication1\App_Data\DB4ESunBank.mdf;Integrated Security=True;MultipleActiveResultSets=True;Application Name=EntityFramework"; }
+
+        public List<T> getList(T oT)
+        {
+            using (var connection = new SqlConnection(SqlConnStr))
+            {
+                var oTResult = connection.Query<T>($@"Select * FROM {oT.GetType().Name.Replace("Model", "")}").ToList();
+
+                return oTResult;
+            }
+        }
+    }
+}
