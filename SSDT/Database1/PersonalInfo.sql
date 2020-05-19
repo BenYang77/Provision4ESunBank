ab9662718c34baa3a5480a33baa0493dd6ff7336
BenYang@LAPTOP-9AGC2Q57 <ben.yang77@gmail.com>
2020-05-19 15:35:46 +0800

    add WebApplication1/App_Data/DB4ESunBank.mdf then publish from SSDT(Database1.sqlproj) to DB4ESunBank.mdf

---
+++ SSDT/Database1/PersonalInfo.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[PersonalInfo]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [security_id] NVARCHAR(50) NOT NULL, 
+    [name] NVARCHAR(50) NOT NULL, 
+    [birthdate] DATE NOT NULL, 
+    [add_date] DATETIME NOT NULL DEFAULT getdate(), 
+    [upd_date] DATETIME NULL
+)
